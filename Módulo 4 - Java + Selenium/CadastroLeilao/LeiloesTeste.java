02f1f188050e0d4703d80fb69cddaba3697cd842
Jader Machado <jadermachado@live.com>
2022-03-25 18:50:40 -0300

    Testes na página leiloes

---
+++ Módulo 4 - Java + Selenium/CadastroLeilao/LeiloesTeste.java
@@ -0,0 +1,47 @@
+package leiloes;
+
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import alura.LoginPage;
+
+public class LeiloesTeste {
+
+	private LeiloesPage paginaDeLeiloes;
+	private CadastroLeilaoPage paginaDeCadastro;
+
+	@BeforeEach
+	public void beforeEach() {
+		LoginPage paginaDeLogin = new LoginPage();
+		this.paginaDeLeiloes = paginaDeLogin.preencheFormulárioLogin("Fulano","pass");
+		this.paginaDeCadastro = paginaDeLeiloes.carregarFormulario();
+	}
+	
+	@AfterEach
+	public void afterEach() {
+		paginaDeLeiloes.fechar();
+	}
+	
+	@Test
+	public void cadastraNovoLeilao() {
+		String hoje = LocalDate.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+		String nome = "Leilão do dia " + hoje;
+		String valor = "500.00";
+		this.paginaDeLeiloes =  paginaDeCadastro.cadastrarLeilao(hoje, nome, valor);
+		Assert.assertTrue(paginaDeLeiloes.isLeilaoCadastrado(hoje, nome, valor));
+	}
+	
+	@Test
+	public void deveriaCadastroLeilaoSemData() {
+		this.paginaDeLeiloes =  paginaDeCadastro.cadastrarLeilao("", "", "");
+		Assert.assertTrue(paginaDeLeiloes.isPaginaAtual());
+		Assert.assertTrue(paginaDeLeiloes.isMensagensVisiveis());
+	}
+
+}
