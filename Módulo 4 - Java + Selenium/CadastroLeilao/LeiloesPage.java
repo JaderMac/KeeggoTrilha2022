7169f5cf0bd791146e443028c8cc428fb8fc5f2d
Jader Machado <jadermachado@live.com>
2022-03-25 18:50:18 -0300

    pagina leiloes

---
+++ Módulo 4 - Java + Selenium/CadastroLeilao/LeiloesPage.java
@@ -0,0 +1,47 @@
+package leiloes;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class LeiloesPage {
+	private static final String URL_CADASTRO_LEILOES = "http://localhost:8080/leiloes/new";
+	private static final String URL_LEILOES = "http://localhost:8080/leiloes";
+	private WebDriver browser;
+	
+	
+	public LeiloesPage(WebDriver browser) {
+		this.browser = browser;
+	}
+
+	public void fechar() {
+		this.browser.quit();
+	}
+
+	public CadastroLeilaoPage carregarFormulario() {
+		this.browser.navigate().to(URL_CADASTRO_LEILOES);
+		return new CadastroLeilaoPage(browser);
+	}
+
+	public boolean isLeilaoCadastrado(String hoje, String nome, String valor) {
+		WebElement linhaDaTabela = this.browser.findElement(By.cssSelector("#tabelaLeiloes tbody tr:last-child"));
+		WebElement colunaNome = linhaDaTabela.findElement(By.cssSelector("td:nth-child(1)"));
+		WebElement colunaDataAbertura = linhaDaTabela.findElement(By.cssSelector("td:nth-child(2)"));
+		WebElement colunaValorInicial = linhaDaTabela.findElement(By.cssSelector("td:nth-child(3)"));
+		return colunaNome.getText().equals(nome) && colunaDataAbertura.getText().equals(hoje) 
+		&& colunaValorInicial.getText().equals(valor);
+	}
+
+	public boolean isPaginaAtual() {
+		return this.browser.getCurrentUrl().equals(URL_LEILOES);
+	}
+
+	public boolean isMensagensVisiveis() {
+		String pagesource = this.browser.getPageSource();
+		return pagesource.contains("minimo 3 caracteres") &&	
+				pagesource.contains("não deve estar em branco") && 
+				pagesource.contains("deve ser um valor maior de 0.1") && 
+				pagesource.contains("deve ser uma data no formato dd/MM/yyyy");
+	}
+}
