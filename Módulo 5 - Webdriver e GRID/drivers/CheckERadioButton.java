bf59f27b42435c7e2d2c56824f1d06af53a4faf2
Jader Machado <jadermachado@live.com>
2022-04-06 17:39:48 -0300

    CheckBox e Radio Button

---
+++ Módulo 5 - Webdriver e GRID/drivers/CheckERadioButton.java
@@ -0,0 +1,44 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class CheckERadioButton {
+	@Test
+	public void testeDeRadio() {
+		System.setProperty("webdriver.chrome.driver",	
+				"D://Java Completo//Livro - "
+				+ "Automacao de testes/libs/drivers/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("file:///D:/Java%20Completo/Udemy%20-%20WebDriver%20e%20Grid/Arquivos/componentes.html");
+		driver.findElement(By.id("elementosForm:sexo:0")).click();
+		Assert.assertTrue(driver.findElement(By.id("elementosForm:sexo:0")).isSelected());
+		
+		driver.findElement(By.id("elementosForm:comidaFavorita:0")).click();
+		driver.findElement(By.id("elementosForm:comidaFavorita:0")).isSelected();
+
+		driver.findElement(By.id("elementosForm:comidaFavorita:1")).click();
+		Assert.assertTrue(driver.findElement(By.id("elementosForm:comidaFavorita:1")).isSelected());
+		
+		driver.findElement(By.id("elementosForm:comidaFavorita:2")).click();
+		Assert.assertTrue(driver.findElement(By.id("elementosForm:comidaFavorita:2")).isSelected());
+	}
+	@Test
+	public void testeDeCheck() {
+		System.setProperty("webdriver.chrome.driver",	
+				"D://Java Completo//Livro - "
+				+ "Automacao de testes/libs/drivers/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("file:///D:/Java%20Completo/Udemy%20-%20WebDriver%20e%20Grid/Arquivos/componentes.html");
+		
+		driver.findElement(By.id("elementosForm:comidaFavorita:0")).click();
+		driver.findElement(By.id("elementosForm:comidaFavorita:0")).isSelected();
+
+		driver.findElement(By.id("elementosForm:comidaFavorita:1")).click();
+		Assert.assertTrue(driver.findElement(By.id("elementosForm:comidaFavorita:1")).isSelected());
+		
+		driver.findElement(By.id("elementosForm:comidaFavorita:2")).click();
+		Assert.assertTrue(driver.findElement(By.id("elementosForm:comidaFavorita:2")).isSelected());
+	}
+}
